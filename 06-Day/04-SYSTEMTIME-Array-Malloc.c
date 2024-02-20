643e33f4c3816fcada399eeb3ace059798155e41
edupazzz <61809719+edupazzz@users.noreply.github.com>
2024-02-19 23:23:49 -0300

    Add files via upload
    C struct

---
+++ 06-Day/04-SYSTEMTIME-Array-Malloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <windows.h>
+
+
+void main()
+{
+	WORD *t;
+	
+	t = (WORD *)malloc(16);
+	
+	GetSystemTime(t);
+	
+	printf("%04d-%02d-%02d %02d:%02d:%02d\n", t[0], t[1], t[3], t[4], t[5], t[6]);
+	
+	free(t);
+}
